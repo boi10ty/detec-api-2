2c278830dd0379ff4dea54b31df340012425e831
boi10ty <phaxoay1@gmail.com>
2025-12-31 23:49:37 +0700

    Update config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,5 +1,5 @@
 const config = {
-    token: '8337281894:AAHkl1uW6G8qHhY3VliO3vL35BBg4kNZRUg',
+    token: '8086831835:AAGp7bTGis_7WFTrDtIrzZI03_SOt-bUj2w',
     chat_id: '1922578871',
     noti_token: '',
     noti_chat_id: ''
